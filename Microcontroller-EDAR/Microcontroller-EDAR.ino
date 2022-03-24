865b601cfa472a66be1fcf50b254ac53ba799d1a
Johnathan Sorrentino <johnathan.sorrentino@mail.utoronto.ca>
2022-03-23 22:24:40 -0400

    Corrected Python-Arduino communication Python correctly sends landing gear status and lights are appropriately adjusted.

--- Microcontroller-EDAR/Microcontroller-EDAR.ino
+++ Microcontroller-EDAR/Microcontroller-EDAR.ino
@@ -23,12 +23,74 @@ char *colourList[] = {
   "FF0099"
 };
 
+char *colourListFull[] = {
+  "FF0000",
+  "FF1900",
+  "FF3300",
+  "FF4D00",
+  "FF6600",
+  "FF8000",
+  "FF9900",
+  "FFB300",
+  "FFCC00",
+  "FFE500",
+  "FFFF00",
+  "E6FF00",
+  "CCFF00",
+  "B3FF00",
+  "99FF00",
+  "80FF00",
+  "66FF00",
+  "4DFF00",
+  "33FF00",
+  "1AFF00",
+  "00FF00",
+  "00FF19",
+  "00FF33",
+  "00FF4C",
+  "00FF66",
+  "00FF80",
+  "00FF99",
+  "00FFB3",
+  "00FFCC",
+  "00FFE5",
+  "00FFFF",
+  "00E6FF",
+  "00CCFF",
+  "00B2FF",
+  "0099FF",
+  "0080FF",
+  "0066FF",
+  "004CFF",
+  "0033FF",
+  "0019FF",
+  "0000FF",
+  "1900FF",
+  "3300FF",
+  "4C00FF",
+  "6600FF",
+  "8000FF",
+  "9900FF",
+  "B300FF",
+  "CC00FF",
+  "E600FF",
+  "FF00FF",
+  "FF00E6",
+  "FF00CC",
+  "FF00B3",
+  "FF0099",
+  "FF0080",
+  "FF0066",
+  "FF004D",
+  "FF0033",
+  "FF001A"
+};
+
 
 void setup() {
   // put your setup code here, to run once:
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   Serial.begin(115200);
-  Serial.setTimeout(1);
 }
 
 void loop() {
@@ -37,26 +99,46 @@ void loop() {
   //chargingBlue();
   //randomLightUp();
   int adcPin = A2;
-  int x;
-  int potValue;
-  int brightness;
-
+  int potValue, brightness;
+  unsigned long int x, val;
+  
   potValue = analogRead(adcPin);
   brightness = map(potValue, 0, 1023, 0, 255);
   FastLED.setBrightness(brightness);
   
   if (Serial.available()) {
-    x = Serial.readString().toInt();
+    x = Serial.parseInt();
     Serial.println(x+1);
   }
-  
   switch(x)
     {
+      case 0: setAllLeds(0xff0000); break;
       case 1: flashRed(); break;
       case 2: chargingBlue(); break;
       case 3: randomLightUp(); break;
-      default: chargingBlue(); break;
+      case 4: setAllLeds(0xffffff); break;
+      default: rainbow(); break;//setAllLeds(0xff0089); break;
     }
+}
+
+void setAllLeds(unsigned long int colour) {
+  int n;
+  for (n=0; n< NUM_LEDS; n++){
+    leds[n] = colour;
+  }
+  FastLED.show();
+}
+
+void rainbow( void ) {
+  int n;
+  unsigned long int val;
+  
+  for (n=0; n<NUM_LEDS; n++) {
+    val = strtoul(colourListFull[n], NULL, 16);
+    leds[n] = val;
+  }
+  FastLED.show();
+  delay(20);
 }
 
 void randomLightUp( void) {
--- main.py
+++ main.py
@@ -5,43 +5,25 @@
 import sched, time
 import json     # Needed to read the Status.json file
 import serial	# Needed for serial communication with Arduino
-
-
-def checkFlag(flag,flagToCheck):
-    return flag & flagToCheck == flagToCheck
-
-
-def checkFlagChange(flag,prevflag,flagToCheck): #if flag was previous false and is now true return true.
-    if checkFlag(flag,flagToCheck) and False ==checkFlag(prevflag,flagToCheck):
-        return True
-    else:
-        return False
-    
-
-# shields up, fsd charging, is in danger/ being interdicted, over heating, masslocked, hardpoints deployed
-def viewActiveFlags(flag): #lits all flags that are currently active
-    flags = ["docked","landed","landing Gear Down","shields up", "super cruise", "flight assist off", "hardpoints deployed", "in wing","lights on",
-             "cargo scoop deployed","silent running","scooping fuel","srv handbrake","srv using turret view","Srv turret retracted","srv drive assist",
-             "fsd masslocked","fsd charging","fsd cooldown","low fuel <25%","over heating >100%","has lat long","is in danger","being interdicted",
-             "in mainship","in fighter","in srv","hud in analysis mode","night vision","altitude from average radius","fsd jump","srv High Beam"]
-    print("\tactive flags:")
-    for i in range(len(flags)):
-        if (checkFlag(flag,2**i)):
-            print("\t",flags[i],":","flag",i)
-
-
+import os       # Needed for file size checking
 
 def readStatus():
     # Open the file, store the information to a variable, then close the file
-    f = open("C:\\Users\\johna\\Saved Games\\Frontier Developments\\Elite Dangerous\\Status.json")
+    file_location = "C:\\Users\\Johnathan\\Saved Games\\Frontier Developments\\Elite Dangerous\\Status.json"    
+
+    f = open(file_location)
     statusContent = f.read()
     f.close()
     
     # Save the filetype using its json format
-    print("\tloaded json:",statusContent)
-    info = json.loads(statusContent)    
-    print("\tflags active:" + bin(info['Flags'])) #if a 1 is present, there is a flag there. ex. 1010 shows flags at 8, 2
-    viewActiveFlags(info['Flags'])
+    if (statusContent == ''):
+        #print("\n\n*************Error Reading File***************\n\n")
+        return
+    else:
+        info = json.loads(statusContent)    
+        write_ard(str(info['Flags']&4))
+    
+	
     
     
 arduino = serial.Serial(port = 'COM6', baudrate = 115200, timeout = .1)
@@ -52,31 +34,22 @@ def read_ard():
 	data = arduino.readline().decode('utf-8').rstrip()
 	return data
 
+
+
+
 s = sched.scheduler(time.time, time.sleep)  # create scheduler
-def loop(num):
-    print("*****************\nchecking json...");
-    print(time.time());
-    
-    #readStatus();
-    print("The number is: ")
-    print(num);
-    write_ard(num);
-    time.sleep(2);
-    number = read_ard();
-    
-    print("Number from the Arduino: ")
-    print(number)
-    
-    num1 = input("Enter a number: ")
-    s.enter(1, 1, loop, argument = (num1,));             #calls its self after waiting 1 seconds
+def loop():
+  
+    readStatus();
+    s.enter(0.02, 1, loop);             #calls its self after waiting 1 seconds
 
 def main():
     ## connect to port
     
     time.sleep(1)
-   
-    num1 = input("Enter a number: ")
-    s.enter(1, 1, loop, argument = (num1,));           
+    print("Code Starting...")
+    #num1 = input("Enter a number: ")
+    s.enter(0.02, 1, loop);           
     s.run(); 
 
 main();
